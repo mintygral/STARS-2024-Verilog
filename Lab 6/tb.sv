43170f32738520041b056be27a8ad68c71340eb4
medha <shindem@purdue.edu>
2024-06-06 14:20:38 -0400

    need to figure out the custom cases

--- Lab 6/tb.sv
+++ Lab 6/tb.sv
@@ -10,20 +10,19 @@ module tb;
   initial begin
     $dumpfile("sim.vcd");
     $dumpvars(0, tb);
-    // out = 0;
-    // strobe = 0;
     in = 20'b0;
-    #10;
+    #1;
     // for loop to test possible inputs
-
     for (integer i=0; i<=19; i++) begin
      in[i] = 1;
-      #10;
-      $display("in=%b, out=%b, strobe=%b", in, out, strobe);
+      #1;
+      $display("in=%b, out=%b, strobe=%b", in, out, strobe);  // checked!! this part works!
     end
 
+
     // reset to 0
-    // custom cases
+    // custom cases 
+    
     in = 20'b0;
     $display("in=%b, out=%b, strobe=%b", in, out, strobe);
     in = 20'b01000000001000000000;
@@ -34,11 +33,13 @@ module tb;
 
     in = 20'b00000000000000000011;
     $display("in=%b, out=%b, strobe=%b", in, out, strobe);
+    
     // Output of custom cases
     // in=01000000001000000000, out=10011, strobe=1 
     // in=00000000010000010000, out=10011, strobe=1
     // in=00000000000000000011, out=10011, strobe=1
-    end
+    
+  end
 
 
 endmodule
