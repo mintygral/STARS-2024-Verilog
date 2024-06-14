e744a2f223c75a79ae412fd46b78a964ff22350e
mintygral <mailmedhashinde@gmail.com>
2024-06-14 16:38:07 -0400

    make stopwatch same as top

--- RTL/Stopwatch/source/stop_watch.sv
+++ RTL/Stopwatch/source/stop_watch.sv
@@ -1,7 +1,6 @@
-/*
-    Module Name: stop_watch
-    Description: Very simple stop watch
-*/
+// FPGA Top Level
+
+`default_nettype none
 
  typedef enum logic [2:0] {
     IDLE = 3'b100,
@@ -9,37 +8,124 @@
     CLEAR = 3'b010
  } state_t;
 
+module top (
+  // I/O ports
+  input  logic hz100, reset,
+  input  logic [20:0] pb,
+  output logic [7:0] left, right,
+         ss7, ss6, ss5, ss4, ss3, ss2, ss1, ss0,
+  output logic red, green, blue,
 
-module stop_watch (
-    input logic clk, nRst_i,
-    input logic button_i,
-    output logic [2:0] mode_o,
-    output logic [4:0] time_o
+  // UART ports
+  output logic [7:0] txdata,
+  input  logic [7:0] rxdata,
+  output logic txclk, rxclk,
+  input  logic txready, rxready
 );
+
+  state_t out;
+  logic strobe;
+  logic strobe1 = (current_mode == RUNNING);
+  assign blue = strobe1; // blue strobe lights up when counter is running
+  // allow synchronizer/encoder in order to be able to change states
+  synckey encode (.clk(hz100), .rst(reset), .in(pb[2:0]), .out(out), .strobe(strobe), .strobe1(strobe1));
+
   // fsm will change the mode based on the input from synckey
-//   state_t mode_o;
-  fsm changemode (.clk(clk), .rst(!nRst_i), .keyout(button_i), .state(mode_o));
-//   assign left[2:0] = mode_o; // check that states are switching properly --- checked!!!
+  state_t current_mode;
+  fsm changemode (.clk(strobe), .rst(reset), .keyout(out), .state(current_mode));
+  assign left[2:0] = current_mode; // check that states are switching properly --- checked!!!
   
   // divide hz100 by 100 to get output of 1x/second
   logic clksecond;
   logic [99:0] clkmax;
   assign clkmax = 100;
   logic pulse;
-  clock_divider #(.N(100)) divideby100 (.clk(clk), .rst(!nRst_i), .max(clkmax), .pulse(pulse));
+  clock_divider #(.N(100)) divideby100 (.clk(hz100), .rst(reset), .max(clkmax), .pulse(pulse));
 
   logic [4:0] max;
   assign max = 5'b11111;
   logic [4:0] count;
   logic at_max;
   // instantiate counter to begin when the mode is running
-  counter #(.N(5)) counter8 (.clk(pulse), .nrst(nRst_i), .enable(mode_o == RUNNING), .clear(mode_o == CLEAR), .wrap(1), 
+  counter #(.N(5)) counter8 (.clk(pulse), .nrst(!reset), .enable(current_mode == RUNNING), .clear(current_mode == CLEAR), .wrap(1), 
           .max(max), .count(count), .at_max(at_max));
+  assign right[4:0] = count; // display number in binary
+  
+  // this MAY be used in order to display on all the displays
+  // logic [55:0] all_displays;
+  // assign all_displays = {ss7[6:0], ss6[6:0], ss5[6:0], ss4[6:0], ss3[6:0], ss2[6:0], ss1[6:0], ss0[6:0]};
+
+  // display count (set max to 9 for now to limit to one segment display)
+  ssdec displaycount(.in(count), .enable(1), .out(ss0[6:0]));
+  ssdec2 displaytens(.in(count), .enable(1), .out(ss1[6:0]));
 endmodule
+
+// encoder
+module synckey (
+    input clk, rst, // clock and reset ports
+    input logic [2:0] in,
+    output logic [2:0] out,
+    output logic strobe,
+    output logic strobe1
+  );
+    logic Q;
+    //logic strobe1;
+
+    //assign strobe = Q[1];
+    // flip-flop 1
+    // this takes the input of the OR'd value of the button inputs as data
+    // outputs Q
+    always_ff @( posedge clk, posedge rst ) begin 
+      if(rst) begin
+        Q <= 1'b0;
+      end
+      else begin
+        Q <= | in;
+      end
+    end
+
+    //flip-flop 2
+    // input is Q (from first flipflop)
+    // output is strobe
+    always_ff @( posedge clk, posedge rst ) begin 
+      if(rst) begin
+        strobe <= 1'b0;
+      end
+      else begin
+        strobe <= Q;
+      end
+    end
+
+    always @(in) begin
+        out = 3'b0;
+        case (in)
+            3'b100: out = 3'b100; // IDLE
+            3'b010: out = 3'b010; // CLEAR
+            3'b001: out = 3'b001; // RUNNING
+            // all other cases:
+            3'b000: out = out;
+            3'b011: out = out;
+            3'b110: out = out;
+            3'b111: out = out;
+            3'b101: out = out;
+            default: out = out; // Default case
+        endcase
+        if (out != 0) begin
+            strobe1 = 1;
+        end
+        if (out == 0) begin
+          strobe1 = 0;
+        end
+        if (out == 0 && (in != 3'b001 | in != 3'b000)) begin
+          strobe1 = 1;
+        end
+        else begin strobe1 = strobe1; end
+    end
+  endmodule
 
 module fsm (
     input logic clk, rst,
-    input logic keyout,
+    input logic [2:0] keyout,
     output state_t state
   );
 
@@ -52,9 +138,9 @@ module fsm (
     always_comb begin 
       next_state = state;
       case(state)
-      IDLE: if (keyout) begin next_state = RUNNING; end else begin next_state = IDLE; end
-      RUNNING: if (keyout) begin next_state = CLEAR; end else begin next_state = RUNNING; end
-      CLEAR: if (keyout) begin next_state = IDLE; end else begin next_state = CLEAR; end
+      IDLE: if (keyout == 3'b001) begin next_state = RUNNING; end else begin next_state = IDLE; end
+      RUNNING: if (keyout == 3'b001) begin next_state = CLEAR; end else begin next_state = RUNNING; end
+      CLEAR: if (keyout == 3'b001) begin next_state = IDLE; end else begin next_state = CLEAR; end
       default: next_state = IDLE;
       endcase
     end
