69c980aa50a3cc16ef403564743152d492c2a10b
medha <shindem@purdue.edu>
2024-06-15 19:10:08 -0400

    ss

--- RTL/Stopwatch/source/synckey.sv
+++ RTL/Stopwatch/source/synckey.sv
@@ -34,34 +34,34 @@ module synckey (
       end
     end
     
-    if (in) begin assign out = 1; end
+    // if (in) begin assign out = 1; end
 
-    // always @(in) begin
-    //     out = 3'b0;
-    //     if (in) begin out = IDLE; end
+    always @(in) begin
+        out = 3'b0;
+        if (in) begin out = 1; end
 
 
-    //     // case (in)
-    //     //     3'b100: out = 3'b100; // IDLE
-    //     //     3'b010: out = 3'b010; // CLEAR
-    //     //     3'b001: out = 3'b001; // RUNNING
-    //     //     // all other cases:
-    //     //     3'b000: out = out;
-    //     //     3'b011: out = out;
-    //     //     3'b110: out = out;
-    //     //     3'b111: out = out;
-    //     //     3'b101: out = out;
-    //     //     default: out = out; // Default case
-    //     // endcase
-    //     if (out != 0) begin
-    //         strobe1 = 1;
-    //     end
-    //     if (out == 0) begin
-    //       strobe1 = 0;
-    //     end
-    //     if (out == 0 && (in != 3'b001 | in != 3'b000)) begin
-    //       strobe1 = 1;
-    //     end
-    //     else begin strobe1 = strobe1; end
-    // end
+        // case (in)
+        //     3'b100: out = 3'b100; // IDLE
+        //     3'b010: out = 3'b010; // CLEAR
+        //     3'b001: out = 3'b001; // RUNNING
+        //     // all other cases:
+        //     3'b000: out = out;
+        //     3'b011: out = out;
+        //     3'b110: out = out;
+        //     3'b111: out = out;
+        //     3'b101: out = out;
+        //     default: out = out; // Default case
+        // endcase
+        if (out != 0) begin
+            strobe1 = 1;
+        end
+        if (out == 0) begin
+          strobe1 = 0;
+        end
+        if (out == 0 && (in)) begin
+          strobe1 = 1;
+        end
+        else begin strobe1 = strobe1; end
+    end
   endmodule
